26101ba5cf91fbd554b915c8746526fde04ae6f1
cahyof30 <f30.cahyo@gmail.com>
2026-02-24 14:29:13 +0700

    Setup CI/CD deployment to Hostdata

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->to('/admin/login');
+});
+
+Route::get('/update-db', function () {
+    Artisan::call('migrate:fresh --force'); // Hati-hati: fresh akan menghapus data lama
+    // Atau gunakan: Artisan::call('migrate --force');
+    return "Database updated!";
 });

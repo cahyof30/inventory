b6cb70ddbfb6946497512a6d12d5b005a8c66501
cahyof30 <f30.cahyo@gmail.com>
2026-02-26 09:06:09 +0700

    Debug Forbidden Login

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../../inventory_app/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */

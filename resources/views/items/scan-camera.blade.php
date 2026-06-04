16175acab02bbcef146542fd5deeea0927aea707
cahyof30 <f30.cahyo@gmail.com>
2026-06-04 14:18:18 +0700

    Add Validation on QR

--- resources/views/items/scan-camera.blade.php
+++ resources/views/items/scan-camera.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Scan QR</title>
-</head>
+
 
       <style>
         body{
@@ -45,7 +45,8 @@
     </style>
 
     	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<body>
+</head>
+            <body>
 <div class="card-wrapper">
 <div class="card" style="display: flex; flex-direction: column;justify-content: center; align-items: center !important;">
 

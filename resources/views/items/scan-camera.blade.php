f3d7ed847d91d58937d4072b7647755c71110a70
cahyof30 <f30.cahyo@gmail.com>
2026-06-04 11:12:55 +0700

    Update scan view

--- resources/views/items/scan-camera.blade.php
+++ resources/views/items/scan-camera.blade.php
@@ -5,7 +5,62 @@
 </head>
 <body>
 
-<h2>Scan QR Asset</h2>
+      <style>
+        body{
+         font-family: Arial, sans-serif;
+    margin: 0;
+    min-height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: #f8f9fa;
+        }
+.card-wrapper {
+            width: 100%;
+            max-width: 600px; 
+            padding: 20px;
+        }
+        .card{
+            border:1px solid #ddd;
+            padding:20px;
+            border-radius:8px;
+        }
+
+        .title{
+            font-size:24px;
+            font-weight:bold;
+            margin-bottom:20px;
+        }
+
+        table{
+            width:100%;
+        }
+
+        td{
+            padding:8px;
+        }
+    </style>
+
+    	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="card-wrapper">
+<div class="card" style="display: flex; flex-direction: column;justify-content: center; align-items: center !important;">
+
+   <div class="title" style="display: flex; justify-content: center; align-items: center;">
+    <img src="{{ asset('storage/'.$item->company?->logo) }}" alt="Logo" style="height:200px; vertical-align:middle;">
+</div>
+    <div class="title">
+     Scan QR Asset
+    </div>
+</div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
 
 <div id="reader" style="width:500px"></div>
 

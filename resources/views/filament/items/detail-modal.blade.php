41adf4a13a0d334475efbbc84014711ea478ce4f
cahyof30 <f30.cahyo@gmail.com>
2026-06-10 14:45:20 +0700

    Fix Item Details Modal

--- resources/views/filament/items/detail-modal.blade.php
+++ resources/views/filament/items/detail-modal.blade.php
@@ -53,10 +53,10 @@
 <div class="card" style="display: flex; flex-direction: column;justify-content: center; align-items: center !important;">
 
    <div class="title" style="display: flex; justify-content: center; align-items: center;">
-    <img src="{{ asset('storage/'.$item->company?->logo) }}" alt="Logo" style="height:100px; vertical-align:middle;">
+    <img src="{{ asset('storage/'.$record->company?->logo) }}" alt="Logo" style="height:100px; vertical-align:middle;">
 </div>
    <div class="title" style="display: flex; justify-content: center; align-items: center;">
-    <img src="{{ $item->image ? asset('storage/'.$item->image) : asset('assets/no_picture.png') }}" alt="Logo" style="height:300px; vertical-align:middle;">
+    <img src="{{ $record->image ? asset('storage/'.$record->image) : asset('assets/no_picture.png') }}" alt="Logo" style="height:300px; vertical-align:middle;">
 </div>
     <div class="title">
         Informasi Aset SGM Group

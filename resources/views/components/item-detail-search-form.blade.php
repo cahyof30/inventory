bcbe3238c36f5e0487bae8fb4eb01249f7a45b2e
cahyof30 <f30.cahyo@gmail.com>
2026-06-18 11:54:42 +0700

    Test Livewire Item Detail

--- resources/views/components/item-detail-search-form.blade.php
+++ resources/views/components/item-detail-search-form.blade.php
@@ -1,27 +1,20 @@
-<div
-    x-data="recaptchaSearch(@this)"
-    class="mb-3"
->
+<div x-data="recaptchaSearch(@this)" class="mb-3">
     <div class="input-group">
+        <form wire:submit="search" class="d-flex w-100">
 
-        <input
-            class="form-control"
-            wire:model.live="code"
-            placeholder="Masukkan kode barang"
-        >
+            <input
+                class="form-control"
+                wire:model.live="code"
+                placeholder="Masukkan kode barang"
+            >
 
-       <button
-    type="button"
-    class="btn btn-primary"
-    wire:submit="search"
->
-    Cari
-</button>
-
+            <button type="submit" class="btn btn-primary">
+                Cari
+            </button>
+        </form>
     </div>
 
     @error('code')
         <div class="text-danger mt-2">{{ $message }}</div>
     @enderror
-
 </div>
